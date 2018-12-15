7ac266dd9fc1bd94dc5d522f381687eaa2bdfea8
ahthzxs <601495817@qq.com>
2018-12-15 21:18:06 +0800

    gateway

---
+++ springcloud-gateway/src/main/java/com/zxs/springcloud/springcloudgateway/SpringcloudGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.zxs.springcloud.springcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class SpringcloudGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudGatewayApplication.class, args);
+    }
+
+}
+
